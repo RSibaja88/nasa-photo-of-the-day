a19ae8ebc4c01fb31b8e1846066931c8f5b62929
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2019-11-22 01:40:09 -0500

    prog

--- src/Components/HeaderCard.js
+++ src/Components/HeaderCard.js
@@ -1,39 +1,22 @@
 import React from "react";
-import {Card, CardBody, CardTitle,} from 'reactstrap';
-
-// const Example = (props) => {
-//     return (
-//       <div>
-//         <Card>
-//           <CardBody>
-//             <CardTitle>Card title</CardTitle>
-//             <CardSubtitle>Card subtitle</CardSubtitle>
-//           </CardBody>
-//           <img width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-//           <CardBody>
-//             <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-//             <CardLink href="#">Card Link</CardLink>
-//             <CardLink href="#">Another Link</CardLink>
-//           </CardBody>
-//         </Card>
-//       </div>
-//     );
-//   };
-
-
-
+import {Card, CardBody, CardTitle, Row, Container} from 'reactstrap';
 
 function Header() {
 
 return (
     <section className="headerBlock">
+    <Container>
     <Card>
     <CardBody>
-        <img src={require("./img/NasaLogo.png")} alt='NASA Logo' height='17%' width='17%' />
+    <Row>
+        <img src={require("./img/NasaLogo.png")} alt='NASA Logo' height='9%' width='9%' />
         <h1>Astronomy Photo of the Day</h1>
         <CardTitle>Call Us: 1800-385-9687</CardTitle>
+        </Row>
         </CardBody>
         </Card>
+        </Container>
+        
     </section>
     );
 
